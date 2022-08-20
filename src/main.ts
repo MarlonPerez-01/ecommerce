f180a37bcc0fad1672590253abeceaf4b8aba4c6
Marlon Pérez <perezmarlon538@gmail.com>
2022-08-20 12:40:32 -0600

    Agrego prefijo a los endpoints

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api/v1');
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
     forbidNonWhitelisted: true,
   }));
+
   await app.listen(process.env.PORT);
 }
 
