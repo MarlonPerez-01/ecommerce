befb9a368397623c48c21869433990accf57a92a
Marlon Pérez <perezmarlon538@gmail.com>
2022-08-20 12:42:23 -0600

    Agrego validacion a variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        CLIENT_URL: Joi.required(),
+        DB_HOST: Joi.required(),
+        DB_NAME: Joi.required(),
+        DB_PASSWORD: Joi.required(),
+        DB_USER: Joi.required(),
+        DB_PORT: Joi.required(),
+        NODE_ENV: Joi.required(),
+        PORT: Joi.required(),
+      }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -15,10 +27,13 @@ import { AppService } from './app.service';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [],
+      autoLoadEntities: true,
       synchronize: process.env.NODE_ENV === 'development',
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+}
