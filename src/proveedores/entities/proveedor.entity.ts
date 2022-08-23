0d1c9defe26abb1d4f540fdd775383dee9210320
MarlonPerez-01 <perezmarlon538@gmail.com>
2022-08-23 11:54:04 -0600

    Agrego entidad proveedor

--- src/proveedores/entities/proveedor.entity.ts
+++ src/proveedores/entities/proveedor.entity.ts
@@ -1,1 +1,36 @@
-export class Proveedor {}
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Proveedor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  primerNombre: string;
+
+  @Column()
+  segundoNombre: string;
+
+  @Column()
+  primerApellido: string;
+
+  @Column()
+  segundoApellido: string;
+
+  @Column()
+  empresa: string;
+
+  @Column()
+  telefono: number;
+
+  @Column()
+  correo: number;
+
+  @DeleteDateColumn({ select: false })
+  deletedAt: Date;
+}
