1eeebb76d3a3de065cd14b9fbc697dbba315c385
MarlonPerez-01 <perezmarlon538@gmail.com>
2022-08-23 11:40:07 -0600

    Exporto CategoriasService

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -8,5 +8,6 @@ import { Categoria } from './entities/categoria.entity';
   imports: [TypeOrmModule.forFeature([Categoria])],
   controllers: [CategoriasController],
   providers: [CategoriasService],
+  exports: [CategoriasService],
 })
 export class CategoriasModule {}
--- src/categorias/dto/update-categoria.dto.ts
+++ src/categorias/dto/update-categoria.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCategoriaDto } from './create-categoria.dto';
 
-export class UpdateCategoriaDto extends PartialType(CreateCategoriaDto) {
-}
+export class UpdateCategoriaDto extends PartialType(CreateCategoriaDto) {}
