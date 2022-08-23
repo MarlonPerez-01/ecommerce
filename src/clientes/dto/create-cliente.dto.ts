450cfcfcfd1b034bc4bd5e9a6a981b600b447acb
MarlonPerez-01 <perezmarlon538@gmail.com>
2022-08-23 12:02:00 -0600

    Agrego entidad cliente

--- src/clientes/dto/create-cliente.dto.ts
+++ src/clientes/dto/create-cliente.dto.ts
@@ -1,1 +1,33 @@
-export class CreateClienteDto {}
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class CreateClienteDto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  primerNombre: string;
+
+  @Column()
+  segundoNombre: string;
+
+  @Column()
+  primerApellido: string;
+
+  @Column()
+  segundoApellido: string;
+
+  @Column()
+  telefono: number;
+
+  @Column()
+  correo: number;
+
+  @DeleteDateColumn({ select: false })
+  deletedAt: Date;
+}
