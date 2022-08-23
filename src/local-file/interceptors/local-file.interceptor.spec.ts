3a28ee6302e7807d85efdde90179d3fb80f9eacc
MarlonPerez-01 <perezmarlon538@gmail.com>
2022-08-23 11:42:07 -0600

    agrego LocalFileInterceptor

---
+++ src/local-file/interceptors/local-file.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { LocalFileInterceptor } from './local-file.interceptor';
+
+describe('LocalFileInterceptor', () => {
+  it('should be defined', () => {
+    expect(new LocalFileInterceptor()).toBeDefined();
+  });
+});
---
+++ src/local-file/interceptors/local-file.interceptor.ts
@@ -0,0 +1,44 @@
+import { extname } from 'path';
+import { randomUUID } from 'crypto';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Injectable, mixin, NestInterceptor, Type } from '@nestjs/common';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+
+interface LocalFileInterceptorOptions {
+  fieldName: string;
+  path?: string;
+}
+
+function LocalFileInterceptor(
+  options: LocalFileInterceptorOptions,
+): Type<NestInterceptor> {
+  @Injectable()
+  class Interceptor implements NestInterceptor {
+    fileInterceptor: NestInterceptor;
+    constructor() {
+      const destination = `${'./uploads'}${options.path}`;
+
+      const multerOptions: MulterOptions = {
+        storage: diskStorage({
+          destination,
+          filename: (req, file, cb) => {
+            cb(null, randomUUID() + extname(file.originalname));
+          },
+        }),
+      };
+
+      this.fileInterceptor = new (FileInterceptor(
+        options.fieldName,
+        multerOptions,
+      ))();
+    }
+
+    intercept(...args: Parameters<NestInterceptor['intercept']>) {
+      return this.fileInterceptor.intercept(...args);
+    }
+  }
+  return mixin(Interceptor);
+}
+
+export default LocalFileInterceptor;
