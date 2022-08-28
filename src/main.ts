b700f69861d2f359b7f5166da8cab4a9d5aca7eb
MarlonPerez-01 <perezmarlon538@gmail.com>
2022-08-28 15:19:41 -0600

    Agrego dependencia swagger y configuro el modulo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('API para utilizar tienda en linea y administrar inventario')
+    .setVersion('1.0')
+    .setBasePath('/api/v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
