7655bce4eb083ee6595c45ebf8b5b6c75b32ba97
MarlonPerez-01 <perezmarlon538@gmail.com>
2022-09-15 12:54:48 -0600

    Agrego interceptor para eliminar propiedad contrasenia

--- src/local-file/interceptors/local-file.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LocalFileInterceptor } from './local-file.interceptor';
-
-describe('LocalFileInterceptor', () => {
-  it('should be defined', () => {
-    expect(new LocalFileInterceptor()).toBeDefined();
-  });
-});
--- src/roles/dto/update-roles.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-
-import { CreateRolesDto } from './create-roles.dto';
-
-export class UpdateRolesDto extends PartialType(CreateRolesDto) {}
